19cdf3b4f0724437d9dcf7622a93d810663d161b
Yasmine C. Fortes <yazzcastrof@gmail.com>
2023-07-09 14:58:42 -0300

    Alterações Reserva

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,17 +15,19 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+      // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+      // *IMPLEMENTE AQUI*
+        bool podeHospedar = Suite.Capacidade >= hospedes.Count;
+        if (podeHospedar)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
-            }
+        // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+        // *IMPLEMENTE AQUI*
+
+          }
         }
 
         public void CadastrarSuite(Suite suite)
