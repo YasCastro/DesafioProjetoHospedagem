acef79b999122dc658018e2b85b924310db7ed2c
Yasmine C. Fortes <yazzcastrof@gmail.com>
2023-07-11 11:50:45 -0300

    Testando capacidade

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 4, valorDiaria: 30);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
 Reserva reserva = new Reserva(diasReservados: 10);
